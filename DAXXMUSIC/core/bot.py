e9e7a61c16c757a55445007020b91784396d209b
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-01 14:26:42 +0530

    Update bot.py

--- DAXXMUSIC/core/bot.py
+++ DAXXMUSIC/core/bot.py
@@ -1,5 +1,3 @@
-import logging
-
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
 
@@ -8,55 +6,49 @@ import config
 from ..logging import LOGGER
 
 
-LOGGER_ID = "-1002168126594"
-
 class DAXX(Client):
     def __init__(self):
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
-        self.logger.info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
             name="DAXXMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
             in_memory=True,
+            parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
         )
 
     async def start(self):
         await super().start()
-        self.me = await self.get_me()
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
 
         try:
-            self.logger.info(f"Trying to send start message to log group/channel {LOGGER_ID}")
             await self.send_message(
-                chat_id=LOGGER_ID,
+                chat_id=config.LOGGER_ID,
                 text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            self.logger.error(
+            LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
             exit()
         except Exception as ex:
-            self.logger.error(
+            LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
             exit()
 
-        a = await self.get_chat_member(LOGGER_ID, self.id)
+        a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            self.logger.error(
+            LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
             exit()
-        self.logger.info(f"Music Bot Started as {self.name}")
+        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
-        
